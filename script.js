7295933350f0fc9973bb3b48fa71757dede4d1f0
Vander-Graaf <riss-2020@mail.ru>
2023-07-03 14:16:30 +0600

    123

---
+++ script.js
@@ -0,0 +1,229 @@
+
+
+const white = '<p class="color">';
+
+
+
+
+// задание 1)
+const firstname = 'Maxim ';
+const lastname = 'Nikulochkin: ';
+
+document.write(white + firstname + lastname);
+console.log(firstname + lastname);
+// задание 1)
+
+
+
+
+// задание 2)
+const num1 = 10;
+const num2 = 10;
+
+ if(num1 === num2){
+    document.write('Числа равны ');
+ }else if (num1 !== num2){
+    document.write('Числа не равны ');
+ }
+// задание 2)
+
+
+
+//задание 3)
+const Color = 1;
+
+
+ if(Color == 1){
+ document.write(white + 'цвет светофора - Красный:' + ' Стоять');
+ } else if (Color == 2) {
+ document.write(white + 'цвет светофора - Желтый:' + ' Ждать');
+ } else if (Color == 3) {
+    document.write(white + 'цвет светофора - Зеленый:' + ' Идти');
+    }
+
+//задание 3)
+
+
+
+
+//Доп задание
+ const Stars = 1;
+    if(Stars == 1){
+    document.write(white + '1 звездочный отель - 20$');
+    } else if (Stars == 2) {
+    document.write(white + '2 звездочный отель - 30$');
+    } else if (Stars == 3) {
+    document.write(white + '3 звездочный отель - 50$');
+    }
+    else if (Stars == 4) {
+    document.write(white + '4 звездочный отель - 70$');
+    }
+    else if (Stars == 5) {
+    document.write(white + '5 звездочный отель - 120$');
+    } else {
+     document.write(white + 'Такого количества звезд нету');
+    }
+//Доп задание
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
